4779c50aef6585d872ca2e77e81f10e05dc047e6
Marton Munkacsi <marton.munkacsi09@gmail.com>
2021-11-26 13:07:41 +0100

    updated url

--- scripts/send-data.js
+++ scripts/send-data.js
@@ -10,7 +10,7 @@ function fetchData() {
 
 
 function sendData(data) {
-  const url = 'http://127.0.0.1:8000/predict_cloud';
+  const url = 'https://detect-fake-news-api-ztj5atks2q-ew.a.run.app/predict_cloud';
   fetch(url, {
     method: 'POST',
     headers: { "Content-Type": "application/json" },
