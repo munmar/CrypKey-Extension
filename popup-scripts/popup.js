86e2f7baa7c7e7805cada505a532b847147efdc6
Marton Munkacsi <marton.munkacsi09@gmail.com>
2021-11-30 22:13:40 +0100

    added Semantic UI features

--- popup-scripts/popup.js
+++ popup-scripts/popup.js
@@ -6,28 +6,34 @@ function prediction_appear() {
   var preddiv = document.querySelector(".box1")
   preddiv.style.visibility = 'visible'
 }
-function gauge() {
-  const changeNum = () => {
-    const randomNum = Math.round(Math.random() * 100);
-    const degrees = Math.round((randomNum / 100) * 180);
-    const root = document.querySelector(":root");
-    let title = document.querySelector(".loader__title");
-    let currentNumber = title.innerText;
-    setInterval(() => {
-      if (currentNumber < randomNum) {
-        currentNumber++;
-        title.innerText = currentNumber;
-      } else if (currentNumber > randomNum) {
-        currentNumber--;
-        title.innerText = currentNumber;
-      }
-    }, 3);
-    root.style.setProperty("--rotation", `${degrees}deg`);
-  };
-  setInterval(() => {
-    changeNum();
-  }, 2000);
+function loader_change() {
+  document.getElementById("MyElement").className =
+    document.getElementById("MyElement").className.replace
+      (/(?:^|\s)active(?!\S)/g, 'disabled')
 }
+
+// function gauge() {
+//   const changeNum = () => {
+//     const randomNum = Math.round(Math.random() * 100);
+//     const degrees = Math.round((randomNum / 100) * 180);
+//     const root = document.querySelector(":root");
+//     let title = document.querySelector(".loader__title");
+//     let currentNumber = title.innerText;
+//     setInterval(() => {
+//       if (currentNumber < randomNum) {
+//         currentNumber++;
+//         title.innerText = currentNumber;
+//       } else if (currentNumber > randomNum) {
+//         currentNumber--;
+//         title.innerText = currentNumber;
+//       }
+//     }, 3);
+//     root.style.setProperty("--rotation", `${degrees}deg`);
+//   };
+//   setInterval(() => {
+//     changeNum();
+//   }, 2000);
+// }
 
 function listenClick() {
   const button = document.getElementById('send-data');
@@ -44,6 +50,7 @@ function listenClick() {
 listenClick();
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   document.getElementById('prediction').innerHTML = parseFloat(parseFloat(message['prediction']*100).toFixed(1)) + '%'
+  loader_change()
   console.log(message)
   sendResponse()
 })
